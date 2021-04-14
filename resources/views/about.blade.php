6350328ad953f3e7643c4a18c04a352cb9a0d8b2
darina <190103482@stu.sdu.edu.kz>
2021-04-14 13:16:10 +0600

    first com

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="EN">
+
+<head>
+    <title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+<h1>ABOUT ME</h1>
+<h4>My name is Darina. I am 19 years old. Now I am studying in SDU. I am a programmist in progress.</h4>
+</body>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="EN">
+
+<head>
+    <title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+<div class="contact">
+<span><i class="fas fa-phone"></i> &nbsp; 8778-575-60-60</span>
+<span><i class="fas fa-envelope"></i> &nbsp; info@190103482.stu.sdu.edu.kz</span>
+</div>
+</body>
